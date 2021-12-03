476cf48539f09205cc4018a1489061c737f12083
M. D. Ahwireng <kaaymike@hotmail.co.uk>
2021-12-03 08:59:28 +0000

    Adds geometry column for address geocoding df

--- modules/utilities.py
+++ modules/utilities.py
@@ -2,24 +2,44 @@
 import geopandas as gpd
 from shapely.geometry import Polygon
 
-def create_grid(bnds, crs):
-    '''This takes a list containing a list of bounds [minx, miny, maxx, maxy] and a string for the crs ESGP:xxxxxx'''
+def create_grid(bnds=None, crs=None, address=None):
+    '''This takes a list containing a list of bounds [minx, miny, maxx, maxy] and a string for the crs ESGP:xxxxxx or only an address'''
     print('RetrieveRegion initialized...')
-
-    espg_val = int(crs.split(':')[-1])
+    
+    try:
+        if address:
+            print('Address parsed. Geocoding initialized...')
+            try:
+                espg_val = 3857
+                grid = gpd.tools.geocode(address, provider='nominatim', user_agent="flow_modeling_app")
+                grid.to_crs(epsg=espg_val, inplace=True)
+                grid['geometry']=grid['geometry'].buffer(200).envelope
+            
+            except Exception as e:
+                print('Error: {}'.format(e))
         
-    MINX, MINY, MAXX, MAXY = bnds
-    poly = Polygon(((MINX, MINY), (MINX, MAXY), (MAXX, MAXY), (MAXX, MINY), (MINX, MINY)))
-    grid = gpd.GeoDataFrame([poly], columns=["geometry"])
-    grid.set_crs(epsg=espg_val, inplace=True)
-    
-    if espg_val != 3857:
-        grid.to_crs(epsg=3857, inplace=True)
-    
-    bnds_ = grid.bounds.loc[0]
-    bnds_lst = [[bnds_['minx'], bnds_['maxx']], [bnds_['miny'], bnds_['maxy']]]
+        else:
+            try:
+                espg_val = int(crs.split(':')[-1])
+                    
+                MINX, MINY, MAXX, MAXY = bnds
+                poly = Polygon(((MINX, MINY), (MINX, MAXY), (MAXX, MAXY), (MAXX, MINY), (MINX, MINY)))
+                grid = gpd.GeoDataFrame([poly], columns=["geometry"])
+                grid.set_crs(epsg=espg_val, inplace=True)
+                
+                if espg_val != 3857:
+                    grid.to_crs(epsg=3857, inplace=True)
 
-    return espg_val, grid, bnds_lst
+            except Exception as e:
+                print('Error: {}'.format(e))
+        
+        bnds_ = grid.bounds.loc[0]
+        bnds_lst = [[bnds_['minx'], bnds_['maxx']], [bnds_['miny'], bnds_['maxy']]]
+
+        return espg_val, grid, bnds_lst
+
+    except Exception as e:
+        print('Error: {}'.format(e))
 
 
 def find_region(ref_gdf, input_gdf):
