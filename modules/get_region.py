d2593086b394b373e00b84cde9a2e0e7bbdc7e95
M. D. Ahwireng <kaaymike@hotmail.co.uk>
2021-12-03 09:01:17 +0000

    Corrects input_gdf

--- modules/get_region.py
+++ modules/get_region.py
@@ -4,15 +4,15 @@ from modules.utilities import create_grid, find_region
 
 class RetrieveRegion():
     '''This class takes bounds and crs from users and returns a region the bounds given by user falls in'''
-    def __init__(self, bnds, crs) -> None:
+    def __init__(self, bnds=None, crs=None, address=None) -> None:
         '''This takes a list containing a list of bounds [minx, miny, maxx, maxy] and a string for the crs ESGP:xxxxxx'''
 
         self.crs = crs
-        
+
         with open( "data/geo_data.pkl", "rb" ) as file:
             self.ref_gdf = pickle.load(file)
         
-        self.bnds, self.espg_val, self.input_gdf = create_grid(bnds=bnds, crs=crs)
+        self.espg_val, self.input_gdf, self.bnds = create_grid(bnds=bnds, crs=crs, address=address)
 
 
     def get_region(self):
