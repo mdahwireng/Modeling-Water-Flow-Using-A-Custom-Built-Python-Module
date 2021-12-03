71ad5aa5d5c00a89b39af91adc5d2e047dd1c466
M. D. Ahwireng <kaaymike@hotmail.co.uk>
2021-12-03 00:06:52 +0000

    Modifies RetrieveRegion to use find_region

--- modules/get_region.py
+++ modules/get_region.py
@@ -3,29 +3,24 @@ import geopandas as gpd
 import pickle
 from shapely.geometry import Polygon
 
-from modules.utilities import create_grid
+from modules.utilities import create_grid, find_region
 
 class RetrieveRegion():
     '''This class takes bounds and crs from users and returns a region the bounds given by user falls in'''
     def __init__(self, bnds, crs) -> None:
         '''This takes a list containing a list of bounds [minx, miny, maxx, maxy] and a string for the crs ESGP:xxxxxx'''
-        print('RetrieveRegion initialized...')
-        
+
         self.crs = crs
         self.ref_gdf = pickle.load( open( "data/geo_data.pkl", "rb" ))
         self.bnds, self.espg_val, self.input_gdf = create_grid(bnds=bnds, crs=crs)
 
 
-
     def get_region(self):
         '''This method checks for regions in which the bounds fall in '''
-        filter = self.ref_gdf['geometry'].contains(self.input_gdf['geometry'][0])
-        if filter.sum() > 1:
-            print('Match acquired!')
-            match_idx = list(self.ref_gdf[filter].index)
-            match_row = self.ref_gdf.loc[match_idx[0]]
-            out_dict={'region':match_row['name'], 'path':match_row['path'], 'bounds':self.bnds}
-            return out_dict
 
-        else:
-            print('Entered bounds did not fall in any of the regions')
+        out_dict = find_region(ref_gdf=self.ref_gdf, input_gdf=self.input_gdf)
+        
+        if out_dict:
+            out_dict['bounds'] = self.bnds
+
+            return out_dict
