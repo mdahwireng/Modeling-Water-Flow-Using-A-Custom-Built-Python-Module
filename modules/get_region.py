5bfc2765d4ef15505612868d159d1cfdf9bedaeb
M. D. Ahwireng <kaaymike@hotmail.co.uk>
2021-12-03 00:12:35 +0000

    Removes unsed imports and load data with context manager

--- modules/get_region.py
+++ modules/get_region.py
@@ -1,7 +1,4 @@
-import pandas as pd
-import geopandas as gpd
 import pickle
-from shapely.geometry import Polygon
 
 from modules.utilities import create_grid, find_region
 
@@ -11,7 +8,10 @@ class RetrieveRegion():
         '''This takes a list containing a list of bounds [minx, miny, maxx, maxy] and a string for the crs ESGP:xxxxxx'''
 
         self.crs = crs
-        self.ref_gdf = pickle.load( open( "data/geo_data.pkl", "rb" ))
+        
+        with open( "data/geo_data.pkl", "rb" ) as file:
+            self.ref_gdf = pickle.load(file)
+        
         self.bnds, self.espg_val, self.input_gdf = create_grid(bnds=bnds, crs=crs)
 
 
